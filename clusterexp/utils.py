440bb79164a0f80a003c9667a211c8893a0c9e11
Natacha, Galmiche <natacha.galmiche@uib.no>
2023-09-15 16:58:56 +0200

    selecting best clusterings methods

---
+++ clusterexp/utils.py
@@ -0,0 +1,66 @@
+import io
+import urllib.request
+from scipy.io import arff
+import pandas as pd
+import numpy as np
+
+URL_ROOT = 'https://raw.githubusercontent.com/nglm/clustering-benchmark/master/src/main/resources/datasets/'
+PATH = URL_ROOT + "artificial/"
+
+# Just one cluster
+UNIMODAL = [
+    "birch-rg1.arff", "birch-rg2.arff",
+    "golfball.arff",
+]
+# No class column in the data
+UNLABELED = [
+    "birch-rg1.arff", "birch-rg2.arff",
+    "birch-rg3.arff",
+    "mopsi-finland.arff", "mopsi-joensuu.arff",
+    "s-set3.arff", "s-set3.arff",
+]
+
+# Unknown number of clusters
+UNKNOWN_K = [
+    "birch-rg3.arff",
+    "mopsi-finland.arff", "mopsi-joensuu.arff",
+    "s-set3.arff", "s-set4.arff",
+]
+
+def arff_from_github(url):
+    ftpstream = urllib.request.urlopen(url)
+    data, meta = arff.loadarff(io.StringIO(ftpstream.read().decode('utf-8')))
+    return data, meta
+
+def load_data_from_github(url, with_labels=True):
+    data, meta = arff_from_github(url)
+    df = pd.DataFrame(data)
+    # Get only data, not the labels and convert to numpy
+    if with_labels:
+        data = df.iloc[:, 0:-1].to_numpy()
+        labels = df.iloc[:, -1].to_numpy()
+    else:
+        data = df.to_numpy()
+        labels = None
+    return data, labels, meta
+
+def get_data_labels(fname, path=PATH):
+    n_labels = None
+    if fname in UNLABELED:
+        with_labels = False
+        if fname in UNIMODAL:
+            n_labels = 1
+        else:
+            n_labels = None
+    else:
+        with_labels = True
+    data, labels, meta = load_data_from_github(
+        path + fname, with_labels=with_labels
+    )
+    N = len(data)
+    if with_labels:
+        n_labels = len(np.unique(labels))
+        if n_labels == N:
+            n_labels = 1
+            labels = np.zeros(N)
+    return data, labels, n_labels, meta
