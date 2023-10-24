8dee9fb769ea15e668f1b6ca5d979c9103b3132a
Natacha, Galmiche <natacha.galmiche@uib.no>
2023-10-24 14:26:20 +0200

    add UCR

--- clusterexp/utils.py
+++ clusterexp/utils.py
@@ -4,7 +4,10 @@ from scipy.io import arff
 import pandas as pd
 import numpy as np
 import os
+import json
 from typing import List, Dict
+
+# --------------------- ClusteringBenchmark ----------------------------
 
 URL_ROOT = 'https://raw.githubusercontent.com/nglm/clustering-benchmark/master/src/main/resources/datasets/'
 
@@ -21,7 +24,7 @@ UNLABELED = [
     "mopsi-finland.arff", "mopsi-joensuu.arff",
     "s-set3.arff", "s-set3.arff",
     # real-world
-    "water-treatment.arff"
+    "water-treatment.arff",
 ]
 
 # Unknown number of clusters
@@ -32,6 +35,26 @@ UNKNOWN_K = [
     "s-set3.arff", "s-set4.arff",
     # real-world
 ]
+
+# Datasets removed, for various reasons (e.g. missing data)
+INVALID = [
+    'dermatology.arff', "segment.arff", "yeast.arff",
+]
+
+
+# ---------------------------- UCR -------------------------------------
+HOME_DIR = os.path.expanduser('~')
+PATH_UCR = f"{HOME_DIR}/Documents/Work/Data/UCR/UCRArchive_2018/"
+
+ILL_FORMATED = [
+    "DodgerLoopDay", "DodgerLoopGame", "DodgerLoopWeekend",
+    "MelbournePedestrian", "AllGestureWiimoteX", "AllGestureWiimoteY",
+    "AllGestureWiimoteZ", "GestureMidAirD1", "GestureMidAirD2",
+    "GestureMidAirD3", "GesturePebbleZ1", "GesturePebbleZ2",
+    "PickupGestureWiimoteZ", "PLAID", "ShakeGestureWiimoteZ",
+    ]
+ILL_FORMATED_DIR = "Missing_value_and_variable_length_datasets_adjusted/"
+
 
 def arff_from_github(url, verbose=False):
     try:
@@ -62,6 +85,17 @@ def load_data_from_github(url, with_labels=True):
         labels = None
     return data, labels, meta
 
+def process_labels(labels):
+    """
+    Give the real number of labels and labels in case of n_labels = N
+    """
+    N = len(labels)
+    n_labels = len(np.unique(labels))
+    if n_labels == N:
+        n_labels = 1
+        labels = np.zeros(N)
+    return labels, n_labels
+
 def get_data_labels(fname, path="./"):
     n_labels = None
     if fname in UNLABELED:
@@ -77,11 +111,19 @@ def get_data_labels(fname, path="./"):
     )
     N = len(data)
     if with_labels:
-        n_labels = len(np.unique(labels))
-        if n_labels == N:
-            n_labels = 1
-            labels = np.zeros(N)
+        labels, n_labels = process_labels(labels)
     return data, labels, n_labels, meta
+
+def get_data_labels_UCR(fname, path="./"):
+
+    df = pd.read_csv(fname, sep="\t")
+
+    data = np.expand_dims(df.iloc[:, 1:].to_numpy(), axis=1)
+
+    labels = df.iloc[:, 0].to_numpy()
+    labels, n_labels = process_labels(labels)
+
+    return data, labels, n_labels, None
 
 def get_list_datasets(fname: str) -> List[str]:
     """
@@ -125,3 +167,46 @@ def get_list_exp(
             if dataset_name + ".json" in fname
         ]
     return fnames
+
+def load_json(fname: str) -> Dict:
+    def object_hook(json_dict):
+        return {
+            int(k) if k.isdigit() else k: v
+            for (k, v) in json_dict.items()
+        }
+    with open(fname) as f_json:
+        d = json.load(f_json, object_hook=object_hook)
+    return d
+
+def write_list_datasets(fname:str, lines: List[str]) -> None:
+    with open(fname, 'w') as f:
+        f.write('\n'.join(lines))
+
+def get_fname(
+    d: str,
+    only_root: bool=False,
+    data_source: str = "artificial",
+) -> str:
+    """
+    Find the filename (or root) corresponding to the UCR dataset
+
+    :param d: dataset name
+    :type d: str
+    :param only_root: defaults to False
+    :type only_root: bool, optional
+    :param data_source: Dataset source ("UCR", "artificial" or
+        "real-world")
+    :type data_source: str
+    :return: The filename (or root) corresponding to the dataset
+    :rtype: str
+    """
+    fname = ""
+    if data_source == "UCR":
+        if d in ILL_FORMATED:
+            fname += f"{ILL_FORMATED_DIR}"
+        fname += f"{d}/"
+        if not only_root:
+            fname += f"{d}_TRAIN.tsv"
+    else:
+        fname += f"{d}"
+    return fname
