66e166b9b549f94a85852dd3998f4bdd03e619a8
Natacha, Galmiche <natacha.galmiche@uib.no>
2023-10-24 17:13:53 +0200

    using utils

--- scripts/select_experiments.py
+++ scripts/select_experiments.py
@@ -2,17 +2,25 @@
 
 import os
 import shutil
+import sys
 import numpy as np
 import json
+from datetime import date
 from pycvi.vi import variational_information
 
-from clusterexp.utils import load_data_from_github, get_data_labels
+from clusterexp.utils import (
+    load_data_from_github, get_data_labels, get_list_datasets,
+    get_list_exp, URL_ROOT
+)
 
 
-RES_DIR = './res/'
-URL_ROOT = 'https://raw.githubusercontent.com/nglm/clustering-benchmark/master/src/main/resources/datasets/'
-PATH = URL_ROOT + "artificial/"
-FNAME_DATASET_EXPS = "datasets_experiments.txt"
+DATA_SOURCE = "artificial"
+#DATA_SOURCE = "real-world"
+
+RES_DIR = f'./res/{DATA_SOURCE}/'
+PATH = f"{URL_ROOT}{DATA_SOURCE}/"
+FNAME_DATASET_EXPS = f"datasets_experiments-{DATA_SOURCE}.txt"
+
 SELECTED_DIR = RES_DIR + "Selected/"
 
 def main():
@@ -21,27 +29,21 @@ def main():
     based of the variation of information with the true clustering
     """
 
-    # List of directories, corresponding to clustering methods
-    list_dirs = [
-        dname.strip() for dname in next(os.walk(RES_DIR))[1]
-        if dname not in  ["Selected", "KMedoids"]]
-    with open(RES_DIR + FNAME_DATASET_EXPS) as f:
-        datasets = f.read().splitlines()
+    datasets = get_list_datasets(RES_DIR + FNAME_DATASET_EXPS)
+
+    # -------------- Redirect output --------------
+    today = str(date.today())
+    out_fname = './output-select-' + today + ".txt"
+
+    fout = open(out_fname, 'wt')
+    sys.stdout = fout
 
     for d in datasets:
 
         # For each dataset, find all experiments working on this dataset
         # but using different clustering methods (by filtering on the
         # filename)
-        fnames = []
-        for dir in list_dirs:
-
-            dir_fnames = [f.name for f in os.scandir(RES_DIR + dir)]
-            # get full directory + filename without the extension
-            fnames += [
-                dir + "/" + fname[:-5] for fname in dir_fnames
-                if d + ".json" in fname
-            ]
+        fnames = get_list_exp(dataset_name=d, res_dir=RES_DIR)
 
         # Storing the variation of information of each experiment
         VIs = []
@@ -51,7 +53,13 @@ def main():
         classes = np.unique(labels)
 
         # true clusters
-        true_clusters = [labels == classes[i] for i in range(n_labels)]
+        # List[List[int]]
+        indices = np.arange(len(data))
+        true_clusters = [
+            indices[labels == classes[i]] for i in range(n_labels)
+        ]
+
+        print(fnames)
 
         for fname in fnames:
 
@@ -67,14 +75,25 @@ def main():
             # Compute VI between the true clustering and each clustering
             # obtained with the different clustering method with the
             # real number of clusters
-            VIs.append(variational_information(true_clusters, best_clusters))
+            VI = variational_information(true_clusters, best_clusters)
+            VIs.append(VI)
+            print(fname, VI)
+
+            # Add VI to the exp file
+            exp["VI"] = VI
+            json_str = json.dumps(exp, indent=2)
+            with open(RES_DIR+fname + ".json", 'w', encoding='utf-8') as f_json:
+                f_json.write(json_str)
 
         # Keep only the clustering method with the lowest VI and copy
         # the experiment file and the figures to another folder
         argbest = np.argmin(VIs)
         fname = fnames[argbest]
+        print(fname)
         shutil.copy(RES_DIR + fname + ".json", SELECTED_DIR)
         shutil.copy(RES_DIR + fname + ".png", SELECTED_DIR)
+
+    fout.close()
 
 if __name__ == "__main__":
     main()
