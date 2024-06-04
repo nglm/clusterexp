39804d427e471ac269cafef18e9d4455f9a16756
Natacha, Galmiche <natacha.galmiche@uib.no>
2024-06-04 16:05:22 +0200

    update analyse_exp to released version of pycvi

--- scripts/analyse_experiments.py
+++ scripts/analyse_experiments.py
@@ -7,55 +7,113 @@ import json
 from datetime import date
 from pycvi.cvi import CVIs
 from math import exp
+import argparse
+from multiprocessing import Process
 
 from clusterexp.utils import (
-    get_list_datasets, URL_ROOT,
-    get_list_exp
+    get_list_datasets, get_list_exp,
+    URL_ROOT, PATH_UCR_LOCAL, PATH_UCR_REMOTE
 )
 
-DATA_SOURCE = "artificial"
-#DATA_SOURCE = "real-world"
+def define_globals(source_number: int = 0, local=True):
+    """
+    Dirty way of coding in order to parallelize the processes.
 
-RES_DIR = f'./res/{DATA_SOURCE}/'
-PATH = f"{URL_ROOT}{DATA_SOURCE}/"
-FNAME_DATASET_EXPS = f"datasets_experiments-{DATA_SOURCE}.txt"
-# Store information dataset per dataset
-FNAME_ANAL_DATASETS = f"analysis_datasets-{DATA_SOURCE}"
-# Store information score per score
-FNAME_ANAL_SCORES = f"analysis_scores-{DATA_SOURCE}"
+    :param source_number: index in ["artificial", "real-world", "UCR"]
+    :type source_number: int, optional
+    :param local: to know whether the local path should be used or not,
+        defaults to True
+    :type local: bool, optional
+    """
+    global DATA_SOURCE, RES_DIR, PATH
+    global FNAME_DATASET_EXPS, FNAME_ANAL_DATASETS, FNAME_ANAL_CVIS
+    global SEED, PATH_UCR
 
-def f_quality(VI):
+    sources = ["artificial", "real-world", "UCR"]
+    DATA_SOURCE = sources[source_number]
+
+    RES_DIR = f'./res/{DATA_SOURCE}/'
+
+    PATH = f"{URL_ROOT}{DATA_SOURCE}/"
+    FNAME_DATASET_EXPS = f"datasets_experiments_theory-{DATA_SOURCE}.txt"
+
+    # Store information dataset per dataset
+    FNAME_ANAL_DATASETS = f"analysis_datasets-{DATA_SOURCE}"
+    # Store information CVI per CVI
+    FNAME_ANAL_CVIS = f"analysis_CVIs-{DATA_SOURCE}"
+
+    SEED = 221
+
+    if local:
+        PATH_UCR = PATH_UCR_LOCAL
+    else:
+        PATH_UCR = PATH_UCR_REMOTE
+
+
+def f_quality(VI: float) -> float:
+    """
+    Quality of a clustering.
+
+    The quality of a predicted clustering is based on the VI between the
+    true and predicted clusterings. We define the quality as:
+
+    :math:`quality = exp(-2*VI)`
+
+    Which means that the quality is between 0 and 1, with higher quality
+    values meaning better clusterings.
+
+    Parameters
+    ----------
+    VI : float
+        The Variation of Information between the true clustering and the
+        predicted clustering
+
+    Returns
+    -------
+    float
+        The quality of the predicted clustering.
+    """
     return exp(-2*VI)
 
 def main():
     """
     Analyse the experiments by making statistics on each dataset and
-    each score.
+    each CVI.
 
-    For each score find:
-    - the number of successful datasets
-    - the number of successful datasets weighted by the quality
-    - the quality of the successful dataset
-    - the max quality that could have been obtained with the given
-    clusterings.
+    For each CVI find:
+    - `acc`: The number of successful datasets
+    - `weighted_acc`: The number of successful datasets weighted by the
+    quality.
+    - `quality`: The accumulated quality of the successful dataset
 
-    For each dataset find the number of successful scores (weighted with
-    the VI)
-    - the number of successful scores
-    - the number of successful scores weighted by the quality
-    - the quality of the successful scores
-    - the max quality that could have been obtained with the given
-    clusterings.
+    For each dataset find:
+    - `acc`: The number of successful CVIs, using all clustering methods
+    - `weighted_acc`: The number of successful CVIs weighted by the
+    quality, using all clustering methods
+    - `k_true_quality`: The accumulated quality of the clustering
+    assuming k_true clusters, using all clustering methods
+    - `quality`: The accumulated quality of the successful CVIs, using
+    all clustering methods
+    - `max_quality`: The accumulated max quality that could have been
+    obtained with the given clusterings, using all clustering methods
+    - `CVIs`: A summary of the performance of each CVI, using all
+    clustering methods (acc, weighted_acc, k_true_quality, quality,
+    max_quality, VIs_selected, VIs_true, VIs_best, success)
+    - `exps`: A summary of the performance of each clustering method,
+    using all CVIs (acc, weighted_acc, quality, k_true_quality,
+    max_quality, VI_true, VI_best)
+    - `k_true`: The true number of clusters
+    - `k_best`: The number of clusters that minimises VI for each
+    clustering method
 
     Note that if a dataset has few successes and/or has a very low
-    accumulated weighted VI, then it means that this dataset was just
+    accumulated quality, then it means that this dataset was just
     too hard to cluster or their definition of cluster might be weird.
 
-    A score that has an unusual high accumulated VI compared to its
+    A CVI that has an unusual high accumulated quality compared to its
     number of success means that it is good at finding relevant clusters
     in general, but not necessarily *the* pre-defined number of clusters
     """
-
 
     res_datasets = {}
 
@@ -72,33 +130,38 @@ def main():
         # "sensible" the true clustering was compared to what is commonly
         # considered as a good clustering by the methods and the CVIs
         res_datasets[d] = {
-            # Global values
+            # Global values (for all CVIs and clustering methods)
             "acc" : 0,            # +1 if k_true=k_selected (indpdt of quality)
             "weighted_acc" : 0,   # +w if k_true=k_selected
             "k_true_quality" : 0, # +w using k_true (indpdt of k_selected)
             "quality" : 0,        # +w using k_selected (indpdt of k_true)
             "max_quality" : 0,    # +w using k that has max quality
             "exps" : {},          # One dict per experience
-            "scores" : {},        # One dict per score
+            "CVIs" : {},          # One dict per CVI
             "k_true" : None,      # k_true of this dataset
-            "k_best" : None,      # k that minimises VI for this dataset
+            "k_best" : [],        # k that minimises VI per clustering method
+            "success" : False,
         }
 
-        # Initialise the accumulators on this dataset for each score
+        # Initialise for each CVI accumulators on this dataset (but for
+        # all clustering methods)
         # Here we gather information on how good this CVI did on this
         # dataset, combining the info of all clustering methods
-        for s in CVIs:
-            for cvi_type in s.cvi_types:
-                res_datasets[d]["scores"][str(score)] = {
+        for cvi in CVIs:
+            for cvi_type in cvi.cvi_types:
+                cvi_instance = cvi(cvi_type=cvi_type)
+                res_datasets[d]["CVIs"][str(cvi_instance)] = {
+                    # Accumulators, shared for all clustering methods
                     "acc" : 0,
                     "weighted_acc" : 0,
                     "k_true_quality" : 0,
                     "quality" : 0,
                     "max_quality" : 0,
-                    "VIs_selected" : [], # VI of k_selected with this score
-                    "VIs_true" : [],     # VI of k_true with this score
+                    # Lists, with one element per clustering method:
+                    "VIs_selected" : [], # VI of k_selected with this CVI
+                    "VIs_true" : [],     # VI of k_true
                     "VIs_best" : [],     # best reachable VI
-                    "success" : [],
+                    "success" : [],      # List of booleans
                 }
 
         # ----------- Going through all clustering methods -------------
@@ -106,13 +169,16 @@ def main():
         # For each dataset, find all experiments working on this dataset
         # but using different clustering methods (by filtering on the
         # filename)
-        fnames = get_list_exp(dataset_name=d, res_dir=RES_DIR)
+        fnames = get_list_exp(
+            dataset_name=d, res_dir=RES_DIR, suffix="_scored.json"
+        )
 
         for fname in fnames:
 
             with open(RES_DIR + fname + ".json") as f_json:
                 exp = json.load(f_json)
 
+            # VI of each assumption k for this clustering method
             # Change None to np.inf, which results in quality=0
             VIS_no_None = {
                 k: vi if vi is not None else np.inf
@@ -126,53 +192,70 @@ def main():
 
             # ------ Initialisation of experience ------
             res_exp = {
+                # Accumulators, shared for all CVIs on this clustering method
                 "acc" : 0,
                 "weighted_acc" : 0,
+                "quality" : 0,
+                # Single values (because all CVIs share the same value)
+                "k_true_quality" : 0,
+                "max_quality" : 0,
                 "VI_true" : VI_true,
                 "VI_best" : VI_best,
             }
 
             res_datasets[d]["k_true"] = k_true
-            res_datasets[d]["k_best"] = k_best
+            res_datasets[d]["k_best"].append(k_best)
 
             k_true_quality = f_quality(VIS_no_None[k_true])
-            max_quality = f_quality(min(VIS_no_None))
+            max_quality = f_quality(VIS_no_None[k_best])
 
-            res_datasets[d]["k_true_quality"] = k_true_quality
-            res_datasets[d]["max_quality"] = max_quality
+            # Update dataset accumulators
+            res_datasets[d]["k_true_quality"] += k_true_quality
+            res_datasets[d]["max_quality"] += max_quality
+
+            # Update clustering method single values
+            res_exp["k_true_quality"] = k_true_quality
+            res_exp["max_quality"] = max_quality
 
             # ------ Update of experience ------
 
-            # Update weighted accuracy for each score of this experiment
-            for s in CVIs:
-                for cvi_type in s.cvi_types:
-                    score = s(cvi_type=cvi_type)
-                    d_score = res_datasets[d]["scores"][str(score)]
+            # Update weighted accuracy for each CVI of this experiment
+            for cvi in CVIs:
+                for cvi_type in cvi.cvi_types:
+                    cvi_instance = cvi(cvi_type=cvi_type)
+                    d_cvi = res_datasets[d]["CVIs"][str(cvi_instance)]
 
                     # Find selected VI of the selected k
-                    k_selected = exp["CVIs"][str(score)]["selected"]
-                    VI_selected = VIS_no_None[k_selected]
+                    k_selected = exp["CVIs"][str(cvi_instance)]["selected"]
+                    if k_selected is None:
+                        VI_selected = np.inf
+                        success = False
+                    else:
+                        VI_selected = VIS_no_None[k_selected]
+                        success = k_selected == k_true
 
-                    d_score["VIs_selected"].append(VI_selected)
-                    d_score["VIs_true"].append(VI_true)
+                    d_cvi["VIs_selected"].append(VI_selected)
+                    d_cvi["VIs_true"].append(VI_true)
+                    d_cvi["VIs_best"].append(VI_best)
                     quality_selected = f_quality(VI_selected)
 
-                    # Compute weighted accuracy for this score
-                    success = k_selected == k_true
-                    d_score["success"].append(success)
+                    # Compute weighted accuracy for this CVI
+                    d_cvi["success"].append(success)
                     if success:
-                        d_score["acc"] += 1
+                        d_cvi["acc"] += 1
                         res_exp["acc"] += 1
-                        d_score["weighted_acc"] += quality_selected
+                        d_cvi["weighted_acc"] += quality_selected
                         res_exp["weighted_acc"] += quality_selected
 
-                    d_score["quality"] += quality_selected
+                    d_cvi["quality"] += quality_selected
                     res_exp["quality"] += quality_selected
 
                     # True and max quality that we could obtain with this
                     # clustering algorithm
-                    d_score["k_true_quality"] += k_true_quality
-                    d_score["max_quality"] += max_quality
+                    d_cvi["k_true_quality"] += k_true_quality
+                    d_cvi["max_quality"] += max_quality
+
+                    res_datasets[d]["CVIs"][str(cvi_instance)] = d_cvi
 
             # ------ Update of dataset ------
 
@@ -182,51 +265,87 @@ def main():
             res_datasets[d]["quality"] += res_exp["quality"]
             res_datasets[d]["exps"][fname] = res_exp
 
-    # ================= Analysis Scores =================
+    # ================= Analysis CVIs =================
 
-    res_scores = {
-        "max_acc" : 0,        # +w using k_true with each experiment
-        "scores" : {},
+    res_CVIs = {
+        # Global:
+        "max_quality" : 0,  # +w using k that has max quality (indpt of CVI)
+        # CVI dependant:
+        "CVIs" : {},        # One dict per CVI
     }
 
-    # ---------------- Initialisation of scores ------------------
+    # ---------------- Initialisation of CVIs ------------------
 
-    # Initialise the accumulators for each score
-    for s in CVIs:
-        for cvi_type in s.cvi_types:
-            res_scores["scores"][str(score)] = {
-                # +1 for each success (regardless of quality)
-                "acc" : 0,
-                # +w (regardless of success) using k_selected
-                "weighted_acc" : 0,
+    # Initialise the accumulators for each CVI
+    for cvi in CVIs:
+        for cvi_type in cvi.cvi_types:
+            cvi_instance = cvi(cvi_type=cvi_type)
+            res_CVIs["CVIs"][str(cvi_instance)] = {
+                "acc" : 0,          # +1 for each success (indpdt of quality)
+                "weighted_acc" : 0, # +w for each success (ie k_true=k_selected)
+                "quality" : 0,      # +w using k_selected (indpdt of k_true)
+
             }
 
     for d in datasets:
 
-        # Update res_scores using res_datasets
-        res_scores["max_acc"] += res_datasets[d]["max_acc"]
+        # Update res_CVIs using res_datasets
+        res_CVIs["max_quality"] += res_datasets[d]["max_quality"]
 
-        # Update accumulators of res_scores after going through all
+        # Update accumulators of res_CVIs after going through all
         # experiments on this dataset
-        for s in CVIs:
-            for cvi_type in s.cvi_types:
-                score = s(cvi_type=cvi_type)
+        for cvi in CVIs:
+            for cvi_type in cvi.cvi_types:
+                cvi_instance = cvi(cvi_type=cvi_type)
 
                 # Shorter variable names for dicts
-                d = res_scores["scores"][str(score)]
-                d_score = res_datasets[d]["scores"][str(score)]
+                d = res_CVIs["CVIs"][str(cvi_instance)]
+                d_cvi = res_datasets[d]["CVIs"][str(cvi_instance)]
 
-                # Update res_scores using res_datasets
-                d["acc"] += d_score["acc"]
-                d["weighted_acc"] += d_score["weighted_acc"]
+                # Update res_CVIs using res_datasets
+                d["acc"] += d_cvi["acc"]
+                d["weighted_acc"] += d_cvi["weighted_acc"]
+                d["quality"] += d_cvi["quality"]
 
     # Save the analysis dict
     json_str = json.dumps(res_datasets, indent=2)
     with open(RES_DIR+FNAME_ANAL_DATASETS+".json", 'w', encoding='utf-8') as f:
         f.write(json_str)
-    json_str = json.dumps(res_scores, indent=2)
-    with open(RES_DIR+FNAME_ANAL_SCORES+".json", 'w', encoding='utf-8') as f:
+    json_str = json.dumps(res_CVIs, indent=2)
+    with open(RES_DIR+FNAME_ANAL_CVIS+".json", 'w', encoding='utf-8') as f:
         f.write(json_str)
 
-if __name__ == "__main__":
+def run_process(source_number, local):
+    define_globals(source_number, local)
     main()
+
+if __name__ == "__main__":
+    CLI=argparse.ArgumentParser()
+    CLI.add_argument(
+        "--local", # Drop `--` for positional/required params
+        nargs=1,  # creates a list of one element
+        type=int,
+        default=0,  # default if nothing is provided
+    )
+    CLI.add_argument(
+        "--source_num", # Drop `--` for positional/required params
+        nargs="*",  # 0 or more values expected => creates a list
+        type=int,
+        default=[0, 1, 2],  # default if nothing is provided
+    )
+    args = CLI.parse_args()
+
+    local = bool(int(args.local[0]))
+    source_numbers = args.source_num
+
+    processes = [
+        Process(target=run_process, args=(i, local))
+        for i in source_numbers
+    ]
+
+    # kick them off
+    for process in processes:
+        process.start()
+    # now wait for them to finish
+    for process in processes:
+        process.join()
