f9cc0abae8b7cf9ec82add5bfdc496723638d3a0
Natacha, Galmiche <natacha.galmiche@uib.no>
2026-07-07 14:06:01 +0200

    filter_experiments tested

--- clusterexp/tests/test_clustering.py
+++ clusterexp/tests/test_clustering.py
@@ -7,7 +7,7 @@ from pycvi.cluster import get_clustering
 
 from clusterexp.config import interpret_config
 from clusterexp.clustering import (
-    decompose_exp_fnames, f_quality, compute_VI_quality, group_exp_by_dataset
+    decompose_exp_fnames, f_quality, compute_VI_quality, group_exp_by_dataset, filter_experiments
 )
 from clusterexp.exp import create_clusterings
 
@@ -105,3 +105,98 @@ def test_group_exp_by_dataset():
     assert len(grouped_exp) == len(datasets)
     # Each experiment is represented
     assert all(any(exp in grouped_exp[d] for d in datasets) for exp in exp)
+
+
+def test_filter_experiments():
+
+    config1 = {
+    "config_data": {
+        "path_data" : "example_data/",
+        "path_res" : "test/test_prepare_data/",
+        "max_n_labels" : 25,
+    },
+    "config_clustering" : {
+        "seed" : 221,
+        "k_range" : [1, 25],
+        "KMeans" : {
+            "model" : "sklearn.cluster.KMeans",
+            "model_kw" : {},
+            "fit_predict_kw" : {},
+            "scaler": "sklearn.preprocessing.StandardScaler",
+            "scaler_kw": {}
+        },
+        "Agglomerative-Single" : {
+            "model": "sklearn.cluster.AgglomerativeClustering",
+            "model_kw": {
+                "linkage": "single",
+                "metric": "euclidean"
+            },
+            "fit_predict_kw": {},
+            "scaler_kw": {}
+        },
+        "KMedoids" : {
+            "model": "kmedoids.KMedoids",
+            "model_kw": {
+                "metric": "euclidean",
+            },
+            "fit_predict_kw": {},
+            "scaler": "sklearn.preprocessing.StandardScaler",
+            "scaler_kw": {}
+        },
+    }
+    }
+
+    config1 = interpret_config(config1)
+    log = create_clusterings(config1)
+
+    exp = log["log_clustering"]["path_exp"]
+    datasets = log["log_data"]["kept_datasets"]
+
+    # -------------- With no constraints ------------------
+    filtered_exp, filtered_datasets = filter_experiments(exp)
+
+    # Check types
+    assert isinstance(filtered_exp, dict)
+    assert isinstance(filtered_datasets, dict)
+    assert isinstance(filtered_exp["kept_experiments"], list)
+    assert isinstance(filtered_exp["dropped_experiments"], dict)
+    assert all(isinstance(v, list) for v in filtered_datasets.values())
+
+    # Check that best q_true and q_best are present
+    assert len(filtered_exp["best_q_true"]) == len(datasets)
+    assert len(filtered_exp["best_q_best"]) == len(datasets)
+
+    # Check that everything is kept when no constraints are given
+    assert len(filtered_exp["kept_experiments"]) == len(exp)
+    assert set(filtered_exp["kept_experiments"]) == set(exp)
+    assert len(filtered_datasets["kept_datasets"]) == len(datasets)
+    assert set(filtered_datasets["kept_datasets"]) == set(datasets)
+
+    # Check that dropped lists are empty when no constraints are given
+    assert all(v == [] for v in filtered_exp["dropped_experiments"].values())
+    assert len(filtered_datasets["dropped_datasets"]) == 0
+
+    # -------------- With constraints ---------------------
+    filtered_exp, filtered_datasets = filter_experiments(
+        exp, best_q_true_only=True
+    )
+
+    # Check types
+    assert isinstance(filtered_exp, dict)
+    assert isinstance(filtered_datasets, dict)
+    assert isinstance(filtered_exp["kept_experiments"], list)
+    assert isinstance(filtered_exp["dropped_experiments"], dict)
+    assert all(isinstance(v, list) for v in filtered_datasets.values())
+
+    # Check that best q_true and q_best are present
+    assert len(filtered_exp["best_q_true"]) == len(datasets)
+    assert len(filtered_exp["best_q_best"]) == len(datasets)
+
+    # Check that all datasets are kept
+    assert len(filtered_datasets["dropped_datasets"]) == 0
+    assert len(filtered_datasets["kept_datasets"]) == len(datasets)
+    assert set(filtered_datasets["kept_datasets"]) == set(datasets)
+
+    # Assert that there are as many kept experiments as there are datasets
+    # since we are keeping only the best experiment for each dataset
+    assert len(filtered_exp["kept_experiments"]) == len(datasets)
